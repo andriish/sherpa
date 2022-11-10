332298f98e325a69770716f5cc17f3f123c80e1d
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2022-11-10 11:26:19 +0100

    OK

--- SherpaWindows/sys/times.h
+++ SherpaWindows/sys/times.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#ifdef _WIN32
+#ifdef WIN32
 #include <sys/timeb.h>
 #include <sys/types.h>
 #include <winsock2.h>
@@ -11,22 +11,6 @@
 //int gettimeofday(struct timeval* t,void* timezone);
 
 
-inline int gettimeofday(struct timeval* t,void* timezone)
-{       struct _timeb timebuffer;
-        _ftime( &timebuffer );
-        t->tv_sec=timebuffer.time;
-        t->tv_usec=1000*timebuffer.millitm;
-		return 0;
-}
-
-inline clock_t times (struct tms *__buffer) {
-
-	__buffer->tms_utime = clock();
-	__buffer->tms_stime = 0;
-	__buffer->tms_cstime = 0;
-	__buffer->tms_cutime = 0;
-	return __buffer->tms_utime;
-}
 
 
 // from linux's sys/times.h
@@ -51,7 +35,24 @@ struct tms
    dead children (and their dead children) in BUFFER.
    Return the elapsed real time, or (clock_t) -1 for errors.
    All times are in CLK_TCKths of a second.  */
-clock_t times (struct tms *__buffer);
+
+
+inline int gettimeofday(struct timeval* t,void* timezone)
+{       struct _timeb timebuffer;
+        _ftime( &timebuffer );
+        t->tv_sec=timebuffer.time;
+        t->tv_usec=1000*timebuffer.millitm;
+		return 0;
+}
+
+inline clock_t times (struct tms *__buffer) {
+
+	__buffer->tms_utime = clock();
+	__buffer->tms_stime = 0;
+	__buffer->tms_cstime = 0;
+	__buffer->tms_cutime = 0;
+	return __buffer->tms_utime;
+}
 
 typedef long long suseconds_t ;
 
